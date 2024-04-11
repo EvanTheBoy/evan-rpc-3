e354610ad2ad6e08f127ceb1c03ceb3583fe93b2
Evan <evanparker945@gmail.com>
2024-04-11 22:48:22 +0800

    完成配置模块代码的编写

--- example-provider/src/main/java/com/evan/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/evan/example/provider/EasyProviderExample.java
@@ -1,5 +1,6 @@
 package com.evan.example.provider;
 
+import com.evan.evanrpc.RpcApplication;
 import com.evan.evanrpc.registry.LocalRegistry;
 import com.evan.evanrpc.server.HttpServer;
 import com.evan.evanrpc.server.VertxHttpServer;
@@ -7,11 +8,14 @@ import com.evan.example.common.service.UserService;
 
 public class EasyProviderExample {
     public static void main(String[] args) {
+        // RPC 框架初始化
+        RpcApplication.init();
+
         // 注册服务
         LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
 
-        // 启动服务
+        // 启动服务器
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
