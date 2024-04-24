78e3a1dd526c404399a81e58fbf09782e8e8dcde
Evan <evanparker945@gmail.com>
2024-04-24 21:15:43 +0800

    正在开发 ZooKeeper 注册中心

--- evan-rpc-core/src/main/java/com/evan/evanrpc/registry/ZooKeeperRegistry.java
+++ evan-rpc-core/src/main/java/com/evan/evanrpc/registry/ZooKeeperRegistry.java
@@ -1,19 +1,72 @@
 package com.evan.evanrpc.registry;
 
+import cn.hutool.core.collection.ConcurrentHashSet;
 import com.evan.evanrpc.config.RegistryConfig;
 import com.evan.evanrpc.model.ServiceMetaInfo;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.x.discovery.ServiceDiscovery;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.details.JsonInstanceSerializer;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ZooKeeperRegistry implements Registry {
+
+    private CuratorFramework client;
+
+    private ServiceDiscovery<ServiceMetaInfo> serviceDiscovery;
+
+    /**
+     * 本机注册的节点 key 集合(用于维护续期)
+     */
+    private final Set<String> localRegisterNodeKeySet = new HashSet<>();
+
+    /**
+     * 注册中心服务缓存
+     */
+    private final RegistryServiceCache registryServiceCache = new RegistryServiceCache();
+
+    /**
+     * 正在监听的 key 集合
+     */
+    private final Set<String> watchingKeySet = new ConcurrentHashSet<>();
+
+    /**
+     * 根节点
+     */
+    private static final String ZK_ROOT_PATH = "/rpc/zk";
+
     @Override
     public void init(RegistryConfig registryConfig) {
+        client = CuratorFrameworkFactory.builder()
+                .connectString(registryConfig.getAddress())
+                .retryPolicy(new ExponentialBackoffRetry(Math.toIntExact(registryConfig.getTimeout()), 3))
+                .build();
+        // 构建 serviceDiscovery 实例
+        serviceDiscovery = ServiceDiscoveryBuilder.builder(ServiceMetaInfo.class)
+                .client(client)
+                .basePath(ZK_ROOT_PATH)
+                .serializer(new JsonInstanceSerializer<>(ServiceMetaInfo.class))
+                .build();
 
+        try {
+            // 启动 client 和 serviceDiscovery
+            client.start();
+            serviceDiscovery.start();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void register(ServiceMetaInfo serviceMetaInfo) throws Exception {
-
+        serviceDiscovery.registerService(buildServiceInstance(serviceMetaInfo));
+        
     }
 
     @Override
@@ -39,5 +92,20 @@ public class ZooKeeperRegistry implements Registry {
     @Override
     public void watch(String serviceNodeKey) {
 
+    }
+
+    private ServiceInstance<ServiceMetaInfo> buildServiceInstance(ServiceMetaInfo serviceMetaInfo) {
+        String serviceAddress = serviceMetaInfo.getServiceHost() + ":" + serviceMetaInfo.getServicePort();
+        try {
+            return ServiceInstance
+                    .<ServiceMetaInfo>builder()
+                    .id(serviceAddress)
+                    .name(serviceMetaInfo.getServiceKey())
+                    .address(serviceAddress)
+                    .payload(serviceMetaInfo)
+                    .build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
