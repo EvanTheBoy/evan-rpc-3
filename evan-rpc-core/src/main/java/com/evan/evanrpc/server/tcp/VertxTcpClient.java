0a02ef2ba0d4a2edea1f12f0fa4ee97c958a4413
Evan <evanparker945@gmail.com>
2024-04-28 21:04:08 +0800

    优化客户端调用代码

--- evan-rpc-core/src/main/java/com/evan/evanrpc/server/tcp/VertxTcpClient.java
+++ evan-rpc-core/src/main/java/com/evan/evanrpc/server/tcp/VertxTcpClient.java
@@ -1,37 +1,73 @@
 package com.evan.evanrpc.server.tcp;
 
+import cn.hutool.core.util.IdUtil;
+import com.evan.evanrpc.RpcApplication;
+import com.evan.evanrpc.model.RpcRequest;
+import com.evan.evanrpc.model.RpcResponse;
+import com.evan.evanrpc.model.ServiceMetaInfo;
+import com.evan.evanrpc.protocol.*;
 import io.vertx.core.Vertx;
+import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetSocket;
 
 import io.vertx.core.buffer.Buffer;
 
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class VertxTcpClient {
-    public void start() {
-        // 创建 Vert.x 实例
+    public static RpcResponse doRequest(RpcRequest rpcRequest, ServiceMetaInfo serviceMetaInfo) throws InterruptedException, ExecutionException {
+        // 发送 TCP 请求
         Vertx vertx = Vertx.vertx();
+        NetClient netClient = vertx.createNetClient();
+        CompletableFuture<RpcResponse> responseFuture = new CompletableFuture<>();
+        netClient.connect(serviceMetaInfo.getServicePort(), serviceMetaInfo.getServiceHost(),
+                result -> {
+                    if (!result.succeeded()) {
+                        System.err.println("Failed to connect to TCP server");
+                        return;
+                    }
+                    NetSocket socket = result.result();
+                    // 发送数据
+                    // 构造消息
+                    ProtocolMessage<RpcRequest> protocolMessage = new ProtocolMessage<>();
+                    ProtocolMessage.Header header = new ProtocolMessage.Header();
+                    header.setMagic(ProtocolConstant.PROTOCOL_MAGIC);
+                    header.setVersion(ProtocolConstant.PROTOCOL_VERSION);
+                    header.setSerializer((byte) ProtocolMessageSerializerEnum.getEnumByValue(RpcApplication.getRpcConfig().getSerializer()).getKey());
+                    header.setType((byte) ProtocolMessageTypeEnum.REQUEST.getKey());
+                    // 生成全局请求 ID
+                    header.setRequestId(IdUtil.getSnowflakeNextId());
+                    protocolMessage.setHeader(header);
+                    protocolMessage.setBody(rpcRequest);
 
-        vertx.createNetClient().connect(8888, "localhost", res -> {
-            if (res.succeeded()) {
-                System.out.println("Connected to TCP server");
-                NetSocket socket = res.result();
-                // 发送数据
-                for (int i = 0; i < 1000; i++) {
-                    Buffer buffer = Buffer.buffer();
-                    String str = "Hello, server!Hello, server!Hello, server!";
-                    buffer.appendInt(0);
-                    buffer.appendInt(str.getBytes().length);
-                    buffer.appendBytes(str.getBytes());
-                    socket.write(buffer);
-                }
-                // 接收响应
-                socket.handler(buff -> System.out.println("Received response from server: " + buff.toString()));
-            } else {
-                System.out.println("Failed to connect to TCP server");
-            }
-        });
-    }
+                    // 编码请求
+                    try {
+                        Buffer encodeBuffer = ProtocolMessageEncoder.encode(protocolMessage);
+                        socket.write(encodeBuffer);
+                    } catch (IOException e) {
+                        throw new RuntimeException("协议消息编码错误");
+                    }
 
-    public static void main(String[] args) {
-        new VertxTcpClient().start();
+                    // 接收响应
+                    TcpBufferHandlerWrapper bufferHandlerWrapper = new TcpBufferHandlerWrapper(
+                            buffer -> {
+                                try {
+                                    ProtocolMessage<RpcResponse> rpcResponseProtocolMessage =
+                                            (ProtocolMessage<RpcResponse>) ProtocolMessageDecoder.decode(buffer);
+                                    responseFuture.complete(rpcResponseProtocolMessage.getBody());
+                                } catch (IOException e) {
+                                    throw new RuntimeException("协议消息解码错误");
+                                }
+                            }
+                    );
+                    socket.handler(bufferHandlerWrapper);
+                });
+
+        RpcResponse rpcResponse = responseFuture.get();
+        // 记得关闭连接
+        netClient.close();
+        return rpcResponse;
     }
 }
